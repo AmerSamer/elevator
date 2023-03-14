abf99ff1512a15b88b948904bcfa7d8a4a3b97b0
AmerSamer <73889178+AmerSamer@users.noreply.github.com>
2023-03-15 00:08:10 +0200

    fixed present time

--- src/App.js
+++ src/App.js
@@ -8,41 +8,41 @@ import ButtonsGroup from './components/ButtonsGroup';
 function App() {
   return (
     <>
-    <ElevatorProvider>
-      <div className="text-center">
-        <div id="app-main" className='row w-75 text-center'>
-        <div className='col-1'>
-            <p>9th</p>
-            <p>8th</p>
-            <p>7th</p>
-            <p>6th</p>
-            <p>5th</p>
-            <p>4th</p>
-            <p>3th</p>
-            <p>2th</p>
-            <p>1th</p>
-            <p>0th</p>
-          </div>
-          <div className='col-1'>
-            <Elevator attr={0}/>
-          </div>
-          <div className='col-1'>
-            <Elevator attr={1}/>
-          </div>
-          <div className='col-1'>
-            <Elevator attr={2}/>
-          </div>
-          <div className='col-1'>
-            <Elevator attr={3}/>
-          </div>
-          <div className='col-1'>
-            <Elevator attr={4}/>
-          </div>
-          <div className='col-1'>
-            <ButtonsGroup/>
+      <ElevatorProvider>
+        <div className="text-center">
+          <div id="app-main" className=' row w-75 text-center'>
+            <div className='col-1 fw-bold '>
+              <p>9th</p>
+              <p>8th</p>
+              <p>7th</p>
+              <p>6th</p>
+              <p>5th</p>
+              <p>4th</p>
+              <p>3rd</p>
+              <p>2nd</p>
+              <p>1st</p>
+              <p>0th</p>
+            </div>
+            <div className='col-1'>
+              <Elevator attr={0} />
+            </div>
+            <div className='col-1'>
+              <Elevator attr={1} />
+            </div>
+            <div className='col-1'>
+              <Elevator attr={2} />
+            </div>
+            <div className='col-1'>
+              <Elevator attr={3} />
+            </div>
+            <div className='col-1'>
+              <Elevator attr={4} />
+            </div>
+            <div className='col-1'>
+              <ButtonsGroup />
+            </div>
           </div>
         </div>
-      </div>
       </ElevatorProvider>
     </>
   );
--- src/components/Elevator.jsx
+++ src/components/Elevator.jsx
@@ -9,48 +9,48 @@ function Elevator(props) {
     return (<>
         <div className="bg-light">
             <div className="border col" id="floor-div">
-                <div className={floorsState[9].isElv && floorsState[9].elevatorId.includes(elvId) ? "d-inline" : "d-none"}>
-                    {floorsState[9].elevatorId.includes(elvId) && elevatorsState[elvId].EstimatedTimeArrival === 0 ? null : elevatorsState[elvId].EstimatedTimeArrival}
+                <div className={floorsState[9].isElv && floorsState[9].elevatorId.includes(elvId) ? "d-inline fst-italic" : "d-none"}>
+                    {floorsState[9].elevatorId.includes(elvId) && elevatorsState[elvId].EstimatedTimeArrival === 0 ? null : elevatorsState[elvId].EstimatedTimeArrival + " sec."}
                     <div>
                         <Icon id="elevator-icon" className={floorsState[9].isElv && floorsState[9].elevatorId.includes(elvId) ? (elevatorsState[elvId].inAction && !floorsState[9].isArrived ? "text-red d-inline" : (floorsState[9].isArrived ? "text-green d-inline" : "text-black d-inline")) : "d-none"} />
                     </div>
                 </div>
             </div>
             <div className="border col" id="floor-div">
-                <div className={floorsState[8].isElv && floorsState[8].elevatorId.includes(elvId) ? "d-inline" : "d-none"}>
-                    {floorsState[8].elevatorId.includes(elvId) && elevatorsState[elvId].EstimatedTimeArrival === 0 ? null : elevatorsState[elvId].EstimatedTimeArrival}
+                <div className={floorsState[8].isElv && floorsState[8].elevatorId.includes(elvId) ? "d-inline fst-italic" : "d-none"}>
+                    {floorsState[8].elevatorId.includes(elvId) && elevatorsState[elvId].EstimatedTimeArrival === 0 ? null : elevatorsState[elvId].EstimatedTimeArrival+ " sec."}
                     <div>
                         <Icon id="elevator-icon" className={floorsState[8].isElv && floorsState[8].elevatorId.includes(elvId) ? (elevatorsState[elvId].inAction && !floorsState[8].isArrived ? "text-red d-inline" : (floorsState[8].isArrived ? "text-green d-inline" : "text-black d-inline")) : "d-none"} />
                     </div>
                 </div>
             </div>
             <div className="border col" id="floor-div">
-                <div className={floorsState[7].isElv && floorsState[7].elevatorId.includes(elvId) ? "d-inline" : "d-none"}>
-                    {floorsState[7].elevatorId.includes(elvId) && elevatorsState[elvId].EstimatedTimeArrival === 0 ? null : elevatorsState[elvId].EstimatedTimeArrival}
+                <div className={floorsState[7].isElv && floorsState[7].elevatorId.includes(elvId) ? "d-inline fst-italic" : "d-none"}>
+                    {floorsState[7].elevatorId.includes(elvId) && elevatorsState[elvId].EstimatedTimeArrival === 0 ? null : elevatorsState[elvId].EstimatedTimeArrival+ " sec."}
                     <div>
                         <Icon id="elevator-icon" className={floorsState[7].isElv && floorsState[7].elevatorId.includes(elvId) ? (elevatorsState[elvId].inAction && !floorsState[7].isArrived ? "text-red d-inline" : (floorsState[7].isArrived ? "text-green d-inline" : "text-black d-inline")) : "d-none"} />
                     </div>
                 </div>
             </div>
             <div className="border col" id="floor-div">
-                <div className={floorsState[6].isElv && floorsState[6].elevatorId.includes(elvId) ? "d-inline" : "d-none"}>
-                    {floorsState[6].elevatorId.includes(elvId) && elevatorsState[elvId].EstimatedTimeArrival === 0 ? null : elevatorsState[elvId].EstimatedTimeArrival}
+                <div className={floorsState[6].isElv && floorsState[6].elevatorId.includes(elvId) ? "d-inline fst-italic" : "d-none"}>
+                    {floorsState[6].elevatorId.includes(elvId) && elevatorsState[elvId].EstimatedTimeArrival === 0 ? null : elevatorsState[elvId].EstimatedTimeArrival+ " sec."}
                     <div>
                         <Icon id="elevator-icon" className={floorsState[6].isElv && floorsState[6].elevatorId.includes(elvId) ? (elevatorsState[elvId].inAction && !floorsState[6].isArrived ? "text-red d-inline" : (floorsState[6].isArrived ? "text-green d-inline" : "text-black d-inline")) : "d-none"} />
                     </div>
                 </div>
             </div>
             <div className="border col" id="floor-div">
-                <div className={floorsState[5].isElv && floorsState[5].elevatorId.includes(elvId) ? "d-inline" : "d-none"}>
-                    {floorsState[5].elevatorId.includes(elvId) && elevatorsState[elvId].EstimatedTimeArrival === 0 ? null : elevatorsState[elvId].EstimatedTimeArrival}
+                <div className={floorsState[5].isElv && floorsState[5].elevatorId.includes(elvId) ? "d-inline fst-italic" : "d-none"}>
+                    {floorsState[5].elevatorId.includes(elvId) && elevatorsState[elvId].EstimatedTimeArrival === 0 ? null : elevatorsState[elvId].EstimatedTimeArrival+ " sec."}
                     <div>
                         <Icon id="elevator-icon" className={floorsState[5].isElv && floorsState[5].elevatorId.includes(elvId) ? (elevatorsState[elvId].inAction && !floorsState[5].isArrived ? "text-red d-inline" : (floorsState[5].isArrived ? "text-green d-inline" : "text-black d-inline")) : "d-none"} />
                     </div>
                 </div>
             </div>
             <div className="border col" id="floor-div">
-                <div className={floorsState[4].isElv && floorsState[4].elevatorId.includes(elvId) ? "d-inline" : "d-none"}>
-                    {floorsState[4].elevatorId.includes(elvId) && elevatorsState[elvId].EstimatedTimeArrival === 0 ? null : elevatorsState[elvId].EstimatedTimeArrival}
+                <div className={floorsState[4].isElv && floorsState[4].elevatorId.includes(elvId) ? "d-inline fst-italic" : "d-none"}>
+                    {floorsState[4].elevatorId.includes(elvId) && elevatorsState[elvId].EstimatedTimeArrival === 0 ? null : elevatorsState[elvId].EstimatedTimeArrival+ " sec."}
                     <div>
                         <Icon id="elevator-icon" className={floorsState[4].isElv && floorsState[4].elevatorId.includes(elvId) ? (elevatorsState[elvId].inAction && !floorsState[4].isArrived ? "text-red d-inline" : (floorsState[4].isArrived ? "text-green d-inline" : "text-black d-inline")) : "d-none"} />
                     </div>
@@ -58,32 +58,32 @@ function Elevator(props) {
 
             </div>
             <div className="border col" id="floor-div">
-                <div className={floorsState[3].isElv && floorsState[3].elevatorId.includes(elvId) ? "d-inline" : "d-none"}>
-                    {floorsState[3].elevatorId.includes(elvId) && elevatorsState[elvId].EstimatedTimeArrival === 0 ? null : elevatorsState[elvId].EstimatedTimeArrival}
+                <div className={floorsState[3].isElv && floorsState[3].elevatorId.includes(elvId) ? "d-inline fst-italic" : "d-none"}>
+                    {floorsState[3].elevatorId.includes(elvId) && elevatorsState[elvId].EstimatedTimeArrival === 0 ? null : elevatorsState[elvId].EstimatedTimeArrival+ " sec."}
                     <div>
                         <Icon id="elevator-icon" className={floorsState[3].isElv && floorsState[3].elevatorId.includes(elvId) ? (elevatorsState[elvId].inAction && !floorsState[3].isArrived ? "text-red d-inline" : (floorsState[3].isArrived ? "text-green d-inline" : "text-black d-inline")) : "d-none"} />
                     </div>
                 </div>
             </div>
             <div className="border col" id="floor-div">
-                <div className={floorsState[2].isElv && floorsState[2].elevatorId.includes(elvId) ? "d-inline" : "d-none"}>
-                    {floorsState[2].elevatorId.includes(elvId) && elevatorsState[elvId].EstimatedTimeArrival === 0 ? null : elevatorsState[elvId].EstimatedTimeArrival}
+                <div className={floorsState[2].isElv && floorsState[2].elevatorId.includes(elvId) ? "d-inline fst-italic" : "d-none"}>
+                    {floorsState[2].elevatorId.includes(elvId) && elevatorsState[elvId].EstimatedTimeArrival === 0 ? null : elevatorsState[elvId].EstimatedTimeArrival+ " sec."}
                     <div>
                         <Icon id="elevator-icon" className={floorsState[2].isElv && floorsState[2].elevatorId.includes(elvId) ? (elevatorsState[elvId].inAction && !floorsState[2].isArrived ? "text-red d-inline" : (floorsState[2].isArrived ? "text-green d-inline" : "text-black d-inline")) : "d-none"} />
                     </div>
                 </div>
             </div>
             <div className="border col" id="floor-div">
-                <div className={floorsState[1].isElv && floorsState[1].elevatorId.includes(elvId) ? "d-inline" : "d-none"}>
-                    {floorsState[1].elevatorId.includes(elvId) && elevatorsState[elvId].EstimatedTimeArrival === 0 ? null : elevatorsState[elvId].EstimatedTimeArrival}
+                <div className={floorsState[1].isElv && floorsState[1].elevatorId.includes(elvId) ? "d-inline fst-italic" : "d-none"}>
+                    {floorsState[1].elevatorId.includes(elvId) && elevatorsState[elvId].EstimatedTimeArrival === 0 ? null : elevatorsState[elvId].EstimatedTimeArrival+ " sec."}
                     <div>
                         <Icon id="elevator-icon" className={floorsState[1].isElv && floorsState[1].elevatorId.includes(elvId) ? (elevatorsState[elvId].inAction && !floorsState[1].isArrived ? "text-red d-inline" : (floorsState[1].isArrived ? "text-green d-inline" : "text-black d-inline")) : "d-none"} />
                     </div>
                 </div>
             </div>
             <div className="border col" id="floor-div">
-                <div className={floorsState[0].isElv && floorsState[0].elevatorId.includes(elvId) ? "d-inline" : "d-none"}>
-                    {floorsState[0].elevatorId.includes(elvId) && elevatorsState[elvId].EstimatedTimeArrival === 0 ? null : elevatorsState[elvId].EstimatedTimeArrival}
+                <div className={floorsState[0].isElv && floorsState[0].elevatorId.includes(elvId) ? "d-inline fst-italic" : "d-none"}>
+                    {floorsState[0].elevatorId.includes(elvId) && elevatorsState[elvId].EstimatedTimeArrival === 0 ? null : elevatorsState[elvId].EstimatedTimeArrival+ " sec."}
                     <div>
                         <Icon id="elevator-icon" className={floorsState[0].isElv && floorsState[0].elevatorId.includes(elvId) ? (elevatorsState[elvId].inAction && !floorsState[0].isArrived ? "text-red d-inline" : (floorsState[0].isArrived ? "text-green d-inline" : "text-black d-inline")) : "d-none"} />
                     </div>
